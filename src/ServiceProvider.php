67496497e32e356fbdf21f1963b4fb3e689298f7
josecaseiro <jvcaseiro@hotmail.com>
2024-02-15 12:56:09 +0100

    call migrate in package

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,18 +10,20 @@ class ServiceProvider extends Provider
     public function boot()
     {
         //Artisan::call('db:seed', ['--class' => 'AngolanGeoDatabaseSeeder']);
-        $this->loadMigrationsFrom(__DIR__ . '/migrations');
+        // $this->loadMigrationsFrom(__DIR__ . '/migrations');
 
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/migrations' => database_path('migrations'),
-            ], 'migrations');
-        }
+        // if ($this->app->runningInConsole()) {
+        //     $this->publishes([
+        //         __DIR__ . '/migrations' => database_path('migrations'),
+        //     ], 'migrations');
+        // }
 
         // Executar o seeder aqui
         $this->app->booted(function () {
             // Executar as migrações antes do seeder
-            Artisan::call('migrate');
+            Artisan::call('migrate', [
+                '--path' => __DIR__ . '/migrations',
+            ]);
 
             // Executar o seeder aqui
             Artisan::call('db:seed', ['--class' => 'Josecaseiro\\AngolanGeo\\Seeders\\AngolanGeoDatabaseSeeder']);

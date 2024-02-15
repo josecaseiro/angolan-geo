726cc971f610d9b16916116e7c631e0eba6c3bdc
josecaseiro <jvcaseiro@hotmail.com>
2024-02-15 14:38:46 +0100

    log execução dos comandos executados após instalação

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -9,29 +9,52 @@ class ServiceProvider extends Provider
 {
     public function boot()
     {
-        //Artisan::call('db:seed', ['--class' => 'AngolanGeoDatabaseSeeder']);
-        // $this->loadMigrationsFrom(__DIR__ . '/migrations');
-
-        // if ($this->app->runningInConsole()) {
-        //     $this->publishes([
-        //         __DIR__ . '/migrations' => database_path('migrations'),
-        //     ], 'migrations');
-        // }
-
         // Executar o seeder aqui
         $this->app->booted(function () {
-            // Executar as migrações antes do seeder
+            // Mensagem informativa antes de executar as migrações
+            $this->info('**Running AngolanGeo migrations...**', 'comment');
+
+            // Executar as migrações diretamente do Service Provider
+            $outputMigrate = Artisan::output();
             Artisan::call('migrate', [
                 '--path' => __DIR__ . '/migrations',
             ]);
+            $this->outputCommandResults('Migration', $outputMigrate);
+
+            // Mensagem informativa antes de executar o seeder
+            $this->info('**Running AngolanGeo seeder...**', 'comment');
 
             // Executar o seeder aqui
-            Artisan::call('db:seed', ['--class' => 'Josecaseiro\\AngolanGeo\\Seeders\\AngolanGeoDatabaseSeeder']);
+            $outputSeeder = Artisan::output();
+            Artisan::call('db:seed', [
+                '--class' => 'Josecaseiro\\AngolanGeo\\Seeders\\AngolanGeoDatabaseSeeder'
+            ]);
+            $this->outputCommandResults('Seeder', $outputSeeder);
         });
     }
 
     public function register()
     {
         //
+    }
+
+    // Método auxiliar para exibir mensagens informativas
+    protected function info($message, $style = null)
+    {
+        $styles = [
+            'comment' => 'fg=white;bg=yellow',
+            'info' => 'fg=black;bg=green',
+        ];
+
+        $this->line("<fg=black;bg=white;options=bold>[AngolanGeo]</> <fg={$styles[$style]}>$message</>");
+    }
+
+    // Método auxiliar para exibir resultados dos comandos
+    protected function outputCommandResults($commandName, $output)
+    {
+        if (!empty(trim($output))) {
+            $this->line("\n<fg=black;bg=white;options=bold>[{$commandName} Output]</>");
+            $this->line($output);
+        }
     }
 }

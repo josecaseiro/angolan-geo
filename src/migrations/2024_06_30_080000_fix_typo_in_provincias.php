c2cc7300f413de9913547e805c8eba164ab613c1
caseirokal <jvcaseiro@hotmail.com>
2024-06-30 02:11:52 +0100

    Create 2024_06_30_080000_fix_typo_in_provincias.php

---
+++ src/migrations/2024_06_30_080000_fix_typo_in_provincias.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixTypoInProvincias extends Migration
+{
+    public function up()
+    {
+       if (!Schema::hasColumn('provincias', 'captital')) {
+            Schema::table('provincias', function (Blueprint $table) {
+                $table->dropColumn('captital');
+            });
+        }
+
+        Schema::table(
+            'provincias',
+            function (Blueprint $table) {
+                $table->string('capital')->nullable();
+            }
+        );
+    }
+
+    public function down()
+    {
+         Schema::table('provincias', function (Blueprint $table) {
+                $table->dropColumn('captital');
+            });
+    }
+}
